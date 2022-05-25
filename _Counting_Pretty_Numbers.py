435d4f7262243db6cf98c863f4e1851261db0524
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-05-25 18:15:21 +0530

    n=int(input()) while n:     a,b=map(int,input().split())     c=0     for i in range(a,b+1):         if(i%10==2 or i%10==3 or i%10==9):             c+=1     print(c) n-=1

---
+++ _Counting_Pretty_Numbers.py
@@ -0,0 +1,10 @@
+n=int(input())
+while n:
+    a,b=map(int,input().split())
+    c=0
+    for i in range(a,b+1):
+        if(i%10==2 or i%10==3 or i%10==9):
+            c+=1
+    print(c)
+n-=1
+    
