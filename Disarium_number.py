fe9df5bba1bb2c5dbde3045a3a2aa270e5419a10
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-05-29 22:46:49 +0530

    a=int(input()) sl=len(str(a)) k=a*1 sum=0 while a:     r=a%10     sum=sum+r**sl     sl=sl-1     a=a//10 if(sum==k):     print('True') else:     print('False')

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,13 +1,13 @@
-n=int(input())
-l=len(str(n))
-s=0
-k=n*1
-while n>0:
-    r=n%10
-    s=s+r**l
-    n=n//10
-    l=l-1
-if(s==k):
+a=int(input())
+sl=len(str(a))
+k=a*1
+sum=0
+while a:
+    r=a%10
+    sum=sum+r**sl
+    sl=sl-1
+    a=a//10
+if(sum==k):
     print('True')
 else:
     print('False')
