96191fb6fc3e3902b5cf6f1461cd23555897fe4c
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-06-22 17:43:29 +0530

    def prime(n):     if(n==1):         return 0     for i in range(2,int(n**0.5)+1):         if(n%i==0):             return 0     else:         return 1 n=int(input()) arr=list(map(int,input().split())) sum=0 c=0 for i in arr:     if(prime(i)):         sum=sum+i         c+=1 k=sum/c print("%.2f"%k)

---
+++ Average_of_primes.py
@@ -0,0 +1,18 @@
+def prime(n):
+    if(n==1):
+        return 0
+    for i in range(2,int(n**0.5)+1):
+        if(n%i==0):
+            return 0
+    else:
+        return 1
+n=int(input())
+arr=list(map(int,input().split()))
+sum=0
+c=0
+for i in arr:
+    if(prime(i)):
+        sum=sum+i
+        c+=1
+k=sum/c
+print("%.2f"%k)
