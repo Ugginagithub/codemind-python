b907ff676d05ca12f15b4bb79a145fbfa30cb9b9
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-06-30 20:23:14 +0530

    n,m=map(int,input().split()) b=[] e=0 o=0 for i in range(n):     arr=list(map(int,input().split()))     b.append(i)     for i in arr:         if(i%2==0):             e+=i         else:             o+=i print(e,o)

--- Sum_of_even_and__odd_elements_in_a_matrix.py
+++ Sum_of_even_and__odd_elements_in_a_matrix.py
@@ -3,10 +3,10 @@ b=[]
 e=0
 o=0
 for i in range(n):
-    a=list(map(int,input().split()))
-    b.append(a)
-    for i in a:
-        if i%2==0:
+    arr=list(map(int,input().split()))
+    b.append(i)
+    for i in arr:
+        if(i%2==0):
             e+=i
         else:
             o+=i
