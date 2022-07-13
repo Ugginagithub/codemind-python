f921028d2c6e65bc5e56cc916bd7ee10e0cb9443
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-07-13 19:10:57 +0530

    n=int(input()) for i in range(n,0,-1):     for j in range(1,n+1):         if(i>=j):             print(chr(64+i),end=' ')         else:             print('',end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
 n=int(input())
-for i in range(n):
-    for j in range(n):
-        print(chr(65+i),end=' ')
+for i in range(n,0,-1):
+    for j in range(1,n+1):
+        if(i>=j):
+            print(chr(64+i),end=' ')
+        else:
+            print('',end='')
     print()
