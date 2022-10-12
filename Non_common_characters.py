10ed39e00ccc0247499f355ee0312a3088f455c8
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-10-12 18:58:05 +0530

    x=input().lower() y=input().lower() l=[] c=0 for i in x:     if i not in y:         l.append(i) for i in y:     if i not in x:         l.append(i) for j in sorted(set(l)):     if j!=' ':        c+=1 print(c)

--- Non_common_characters.py
+++ Non_common_characters.py
@@ -1,21 +1,14 @@
-a = input()
-b = input()
-a,b = a.replace(" ",""),b.replace(" ","")
-a,b = a.lower(),b.lower()
-a,b = set(a),set(b)
-a,b = list(a),list(b)
-c = []
-for i in range(0,len(a)):
-    if a[i] not in b:
-        c.append(a[i])
-for i in range(0,len(b)):
-    if b[i] not in a and b[i] not in c:
-        c.append(b[i])
-c.sort()
-c = str(c)
-c = c.replace(",","")
-c = c.replace("'","")
-c = c.replace("[","")
-c = c.replace("]","")
-c = c.replace(" ","")
-print(len(c))
+x=input().lower()
+y=input().lower()
+l=[]
+c=0
+for i in x:
+    if i not in y:
+        l.append(i)
+for i in y:
+    if i not in x:
+        l.append(i)        
+for j in sorted(set(l)):
+    if j!=' ':
+       c+=1
+print(c)
