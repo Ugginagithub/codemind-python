cddf0998291111854537050fc41075cf36957e2a
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-10-12 20:30:41 +0530

    s=input() s=s.split(' ') n=len(s) print(min(s[0]),end=' ') print(max(s[n-1]))

--- small_and_large_elements.py
+++ small_and_large_elements.py
@@ -1,6 +1,5 @@
-n = input()
-m = n.split(" ")
-if len(m)==2:
-    print(min(m[0]),max(m[1]))
-else:
-    print(min(m[0]),max(m[len(m)-2]))
+s=input()
+s=s.split(' ')
+n=len(s)
+print(min(s[0]),end=' ')
+print(max(s[n-1]))
