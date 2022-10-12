8703c9ec01374c6313257b850f8fe4aa4bdce474
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-10-12 22:16:48 +0530

    s=list(map(str,input().split())) c=0 for i in s:     m=i.lower()     n=m[::-1]     if m==n:         c+=1 print(c)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,25 +1,9 @@
-l = input()
-l = l.split(" ")
-c = 0
-for i in range(len(l)):
-    n = l[i].lower()
-    i = 0
-    j = len(n)-1
-    count = 0
-    if len(n)%2!=0:
-        while i<=j:
-            if n[i]==n[j]:
-                count += 1
-            i += 1
-            j -= 1
-        if count==len(n)//2+1:
-            c+=1
-    else:
-        while i<j:
-            if n[i]==n[j]:
-                count += 1
-            i += 1
-            j -= 1
-        if count==len(n)//2:
-            c += 1
+s=list(map(str,input().split()))
+c=0
+for i in s:
+    m=i.lower()
+    n=m[::-1]
+    if m==n:
+        c+=1
 print(c)
+    
