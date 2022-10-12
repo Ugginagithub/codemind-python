18d44c140d0f7913cddbfc559d43e99aa490c811
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-10-12 22:53:27 +0530

    x=list(map(str,input().split())) c=0 d=[] for i in x:     for j in i:         if j=='a' or j=='e' or j=='i' or j=='o' or j=='u':             c+=1         elif j=='A' or j=='E' or j=='I' or j=='O' or j=='U':             c+=1     print(c,end=" ")     c=0

---
+++ vowel_count_in_each_word.py
@@ -0,0 +1,11 @@
+x=list(map(str,input().split()))
+c=0
+d=[]
+for i in x:
+    for j in i:
+        if j=='a' or j=='e' or j=='i' or j=='o' or j=='u':
+            c+=1
+        elif j=='A' or j=='E' or j=='I' or j=='O' or j=='U':
+            c+=1
+    print(c,end=" ")
+    c=0
