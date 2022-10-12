360946664dc8d1c877336826bc6b9f5baf60ec16
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-10-12 18:06:57 +0530

    x=input() for i in sorted(set(x)):     if i>='a' and i<='z':         if x.count(i)==1:             print(i,end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,17 +1,5 @@
-n = input()
-n = n.lower()
-n = n.replace(" ","")
-s = []
-for i in range(0,len(n)):
-    c = n.count(n[i])
-    if c==1:
-        s.append(n[i])
-s.sort()
-s = str(s)
-s = s.replace(",","")
-s = s.replace("[","")
-s = s.replace("]","")
-s = s.replace(",","")
-s = s.replace("'","")
-s = s.replace(" ","")
-print(s)
+x=input()
+for i in sorted(set(x)):
+    if i>='a' and i<='z':
+        if x.count(i)==1:
+            print(i,end="")
