e5d853bb2596cef51781578494a0419f11bec3c7
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-10-12 22:45:11 +0530

    x=list(map(str,input().split())) c=0 d=[] for i in x:     for j in i:         if j=='a' or j=='e' or j=='i' or j=='o' or j=='u':             c+=1         elif j=='A' or j=='E' or j=='I' or j=='O' or j=='U':             c+=1     d.append(c)     c=0 n=max(d) print(d.count(n))

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,13 @@
+x=list(map(str,input().split()))
+c=0
+d=[]
+for i in x:
+    for j in i:
+        if j=='a' or j=='e' or j=='i' or j=='o' or j=='u':
+            c+=1
+        elif j=='A' or j=='E' or j=='I' or j=='O' or j=='U':
+            c+=1
+    d.append(c)
+    c=0
+n=max(d)
+print(d.count(n))
