39a68220ef26d7cd375a592b6155cf60edfed3d2
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-10-12 11:12:34 +0530

    def pri(n):     if n==1:         return 0     for i in range(2,int(n//2)+1):         if n%i==0:             return 0     else:         return 1 n=int(input()) if pri(n):     print("prime") else:     print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,13 @@
-a=int(input())
-c=0
-for i in range(1,a+1):
-    if a%i==0:
-        c=c+1
-if c==2:
-    print('prime')
+def pri(n):
+    if n==1:
+        return 0
+    for i in range(2,int(n//2)+1):
+        if n%i==0:
+            return 0
+    else:
+        return 1
+n=int(input())
+if pri(n):
+    print("prime")
 else:
-    print('not a prime')
+    print("not a prime")
