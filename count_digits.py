0fc4637859073ef956f2791b1cf4278ae01a470e
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-10-13 17:51:01 +0530

    def dig(n):     if n==0:         return 1     if n<0:         n=n*-1     c=0     while n:         i=n%10         n=n//10         c+=1     return c n=int(input()) a=list(map(int,input().split())) for i in range(n):     print(dig(a[i]),end=' ')

--- count_digits.py
+++ count_digits.py
@@ -1,7 +1,15 @@
+def dig(n):
+    if n==0:
+        return 1
+    if n<0:
+        n=n*-1
+    c=0
+    while n:
+        i=n%10
+        n=n//10
+        c+=1
+    return c
 n=int(input())
-arr=list(map(int,input().split()))
-for i in arr:
-    if i<0:
-        print(len(str(i))-1,end=' ')
-    else:
-        print(len(str(i)),end=' ')
+a=list(map(int,input().split()))
+for i in range(n):
+    print(dig(a[i]),end=' ')
