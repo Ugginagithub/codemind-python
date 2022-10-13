fe97d78f9013b9dfdc3b89cc31bd861f2480b1ed
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-10-13 18:26:13 +0530

    a,b=map(int,input().split()) l=list(map(int,input().split())) m=list(map(int,input().split())) for i in sorted(set(l),key=l.index):     if m.count(i)!=0:         print(i,end=" ")

--- Common_elements.py
+++ Common_elements.py
@@ -1,13 +1,6 @@
 a,b=map(int,input().split())
-arr1=list(map(int,input().split()))
-arr2=list(map(int,input().split()))
-b=[]
-c=[]
-for i in arr1:
-    for j in arr2:
-        if(i==j):
-            b.append(i)
-for i in b:
-    if i not in c:
-        c.append(i)
-print(*c)
+l=list(map(int,input().split()))
+m=list(map(int,input().split()))
+for i in sorted(set(l),key=l.index):
+    if m.count(i)!=0:
+        print(i,end=" ")
